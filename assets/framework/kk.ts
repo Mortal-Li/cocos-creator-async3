3536032b58f0563cbc40e651342705b85721f93c
Mortal-Li <sharinglxh@gmail.com>
2024-04-26 14:26:35 +0800

    local mgr

--- assets/framework/kk.ts
+++ assets/framework/kk.ts
@@ -6,10 +6,13 @@
 
 import { find, Node } from "cc";
 import UIManager from "./manager/UIManager";
-
+import LocalManager from "./manager/LocalManager";
+import SoundManager from "./manager/SoundManager";
 
 const kk = {
     uiMgr: new UIManager(),
+    localMgr: new LocalManager(),
+    soundMgr: new SoundManager(),
     
     godNode: <Node> null,
 
