7e0e41570019616ee93b19304c08c564f4e302bc
Mortal-Li <sharinglxh@gmail.com>
2024-07-15 22:03:26 +0800

    comon pop

--- assets/KKCommonBundle/Scripts/KKCommonPopup.ts
+++ assets/KKCommonBundle/Scripts/KKCommonPopup.ts
@@ -4,27 +4,46 @@
  * @created 2024/6/24上午11:01:24
  */
 
-import { _decorator, Component, Node, Event } from 'cc';
+import { _decorator, Component, Node, Event, Label } from 'cc';
 import { PopupBase } from '../../framework/ui/PopupBase';
 const { ccclass, property } = _decorator;
 
 @ccclass('KKCommonPopup')
 export class KKCommonPopup extends PopupBase {
 
+    @property(Node)
+    private closeNd: Node = null;
+
+    @property(Label)
+    private btnLbl: Label = null;
+
+    @property(Label)
+    private descLbl: Label = null;
+
+    @property(Label)
+    private titleLbl: Label = null;
+
     onLoad() {
-        
-    }
-
-    start() {
-
+        let d = this.recvData;
+        if (d.title) this.titleLbl.string = d.title;
+        if (d.desc) this.descLbl.string = d.desc;
+        if (d.btnTxt) this.btnLbl.string = d.btnTxt;
+        this.closeNd.active = !d.hideClose;
     }
 
     onBtnClick(evt: Event, name: string): void {
         switch (name) {
-            case "":
-                
+            case "close":
+                this.ret = false;
+                if (this.recvData.closeCall) this.recvData.closeCall();
+                break;
+
+            case "btn":
+                this.ret = true;
+                if (this.recvData.btnCall) this.recvData.btnCall();
                 break;
         }
+        this.close();
     }
     
 }
